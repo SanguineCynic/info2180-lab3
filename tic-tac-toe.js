ce5719945a71ab00a26f9c5587e680c1b55ba7d3
Jonathan Astwood <jastwood21@gmail.com>
2022-10-07 00:31:53 -0500

    Exercise 5 completed

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -2,7 +2,8 @@ window.onload = function(){
   var board = document.getElementById("board")
   var squares = board.getElementsByTagName("div")
   var counter = 0
-  var boardstate = ["0","0","0","0","0","0","0","0","0"]
+  var boardstate = ["0","1","2","3","4","5","6","7","8"]
+  var status = document.getElementById("status")
 
   for(let i = 0; i < squares.length; i++){
     squares[i].classList.add("square")
@@ -13,12 +14,20 @@ window.onload = function(){
         squares[i].textContent = "X"
         counter++
         boardstate.splice(i,1,"X")
+        if ((boardstate[0] && boardstate [1] == boardstate[2]) || (boardstate[3] && boardstate[4] == boardstate[5]) || (boardstate[6] && boardstate[7] == boardstate[8]) ||  (boardstate[0] && boardstate[3] == boardstate[6]) ||  (boardstate[1] && boardstate[4] == boardstate[7]) ||  (boardstate[2] && boardstate[5] == boardstate[8]) ||  (boardstate[0] && boardstate[4] == boardstate[8]) || (boardstate[2] && boardstate[4] == boardstate[6])){
+          status.textContent = "Congratulations! X is the Winner!"
+          status.classList.add("you-won")
+        }
       }
       else if (counter % 2 == 1 && !(squares[i].classList.contains("X") || squares[i].classList.contains("O"))){
         squares[i].classList.add("O")
         squares[i].textContent = "O"
         counter++
         boardstate.splice(i,1,"O")
+        if ((boardstate[0] && boardstate [1] == boardstate[2]) || (boardstate[3] && boardstate[4] == boardstate[5]) || (boardstate[6] && boardstate[7] == boardstate[8]) ||  (boardstate[0] && boardstate[3] == boardstate[6]) ||  (boardstate[1] && boardstate[4] == boardstate[7]) ||  (boardstate[2] && boardstate[5] == boardstate[8]) ||  (boardstate[0] && boardstate[4] == boardstate[8]) || (boardstate[2] && boardstate[4] == boardstate[6])){
+          status.textContent = "Congratulations! O is the Winner!"
+          status.classList.add("you-won")
+        }
       }
 
     })
@@ -30,6 +39,7 @@ window.onload = function(){
     squares[i].addEventListener('mouseout',function(){
       squares[i].classList.remove("hover")
     })
+
 
 
   }
